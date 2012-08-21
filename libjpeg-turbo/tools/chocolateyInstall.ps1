ec057ee925f464862209bc2d3e557a2de0290629
Iristyle <Iristyle@github>
2012-08-20 22:13:36 -0400

    Added libjpeg-turbo 1.2.1
    - Installs from executable on SourceForge - x86 or x64 based on OS
    - Adds bin directory to PATH (useful for Yeoman)

---
+++ libjpeg-turbo/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+#uses NSIS installer
+Install-ChocolateyPackage 'libjpeg-turbo' 'exe' '/S' `
+  'http://sourceforge.net/projects/libjpeg-turbo/files/1.2.1/libjpeg-turbo-1.2.1-vc.exe/download' `
+  'http://sourceforge.net/projects/libjpeg-turbo/files/1.2.1/libjpeg-turbo-1.2.1-vc64.exe/download'
+
+$systemDrive = [Environment]::GetEnvironmentVariable('SystemDrive')
+$installPath = @('libjpeg-turbo64', 'libjpeg-turbo') |
+  % { Join-Path (Join-Path $systemDrive $_) 'bin' } |
+  ? { Test-Path $_ } |
+  Select -First 1
+
+Install-ChocolateyPath $installPath
