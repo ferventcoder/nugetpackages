4dd29e05b0741db8baf32602afb81f599539a55d
Iristyle <Iristyle@github>
2012-08-20 22:57:23 -0400

    Revised installation to publish success / fail

--- libjpeg-turbo/tools/chocolateyInstall.ps1
+++ libjpeg-turbo/tools/chocolateyInstall.ps1
@@ -1,12 +1,20 @@
-#uses NSIS installer
-Install-ChocolateyPackage 'libjpeg-turbo' 'exe' '/S' `
-  'http://sourceforge.net/projects/libjpeg-turbo/files/1.2.1/libjpeg-turbo-1.2.1-vc.exe/download' `
-  'http://sourceforge.net/projects/libjpeg-turbo/files/1.2.1/libjpeg-turbo-1.2.1-vc64.exe/download'
+try {
+  $package = 'libjpeg-turbo'
+  #uses NSIS installer
+  Install-ChocolateyPackage 'libjpeg-turbo' 'exe' '/S' `
+    'http://sourceforge.net/projects/libjpeg-turbo/files/1.2.1/libjpeg-turbo-1.2.1-vc.exe/download' `
+    'http://sourceforge.net/projects/libjpeg-turbo/files/1.2.1/libjpeg-turbo-1.2.1-vc64.exe/download'
 
-$systemDrive = [Environment]::GetEnvironmentVariable('SystemDrive')
-$installPath = @('libjpeg-turbo64', 'libjpeg-turbo') |
-  % { Join-Path (Join-Path $systemDrive $_) 'bin' } |
-  ? { Test-Path $_ } |
-  Select -First 1
+  $systemDrive = [Environment]::GetEnvironmentVariable('SystemDrive')
+  $installPath = @('libjpeg-turbo64', 'libjpeg-turbo') |
+    % { Join-Path (Join-Path $systemDrive $_) 'bin' } |
+    ? { Test-Path $_ } |
+    Select -First 1
 
-Install-ChocolateyPath $installPath
+  Install-ChocolateyPath $installPath
+
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
